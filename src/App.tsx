7bed8a4093ff2974f092557768f27e93b6aeb45b
IlyaKd <ilyakd@protonmail.com>
2025-07-25 08:59:25 +0000

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ProfileForm } from './ProfileForm/ProfileForm'
+
+function App() {
+  return (
+    <>
+      <div>
+        <ProfileForm />
+      </div>
+    </>
+  )
+}
+
+export default App
